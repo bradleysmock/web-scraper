c08655ec9c825118b50fb54ce496f31f81ab0806
allison-worthy <allison.worthy@wgu.edu>
2023-01-06 08:23:58 -0700

    partnersiteBackup

--- src/crawler/index.js
+++ src/crawler/index.js
@@ -1,8 +1,8 @@
-import HCCrawler from 'headless-chrome-crawler';
-import {saveToFile} from "../shared/saveToFile.js";
+import HCCrawler from "headless-chrome-crawler";
+import { saveToFile } from "../shared/saveToFile.js";
+import {existsSync} from "node:fs";
 
 (async () => {
-
   const errors = [];
 
   const crawler = await HCCrawler.launch({
@@ -13,21 +13,54 @@ import {saveToFile} from "../shared/saveToFile.js";
       const result = await crawl();
 
       try {
-        await page.waitForSelector('a[ng-href]', {timeout: 2000});
+        await page.waitForSelector("a[ng-href]", { timeout: 3000 });
 
-        const links = await page.$$eval(
-            'a[href], a[ng-href]',
-            anchors => {
-              return anchors.map(anchor => anchor.href)
-                .filter(href => href.startsWith('https://partners.wgu.edu') && !href.includes('Authenticate'))
-                .map(href => href.endsWith('#') ? href.slice(0, href.length - 1) : href)
-            }
-        );
+        const photos = await page.$$eval("img", (imgs) => {
+          return imgs.map((x) => x.src);
+        });
+        for (const photo of photos) {
+          console.log(photo);
+          const imagepage = await page.goto(photo);
+
+          const photoName = photo
+            .split("/")
+            .pop()
+            .replace(/\?/g, "-")
+            .replace(/=/g, "-")
+            .replace(/\&/g, "-");
+
+          if (!existsSync("./output/image/" + photoName)){  
+          
+             saveToFile(
+                photoName,
+                "",
+                await imagepage.buffer(),
+                "./output/image"
+              );
+          // await fs.writeFile(photoName, await imagepage.buffer())
+          
+          } else {
+            console.log("skipping", photoName)
+        }
+      }
+
+        const links = await page.$$eval("a[href], a[ng-href]", (anchors) => {
+          return anchors
+            .map((anchor) => anchor.href)
+            .filter(
+              (href) =>
+                href.startsWith("https://partners.wgu.edu") &&
+                !href.includes("Authenticate")
+            )
+            .map((href) =>
+              href.endsWith("#") ? href.slice(0, href.length - 1) : href
+            );
+        });
 
         result.links = [...links];
       } catch (error) {
         // console.error(page.url(), error);
-        errors.push({ url: page.url(), error});
+        errors.push({ url: page.url(), error });
       }
 
       result.html = await page.content();
@@ -36,40 +69,39 @@ import {saveToFile} from "../shared/saveToFile.js";
     },
 
     // Function to be evaluated in browsers
-    evaluatePage: (() => ({
-      title: $('title').text().trim(),
+    evaluatePage: () => ({
+      title: $("title").text().trim(),
       url: window.location.href,
-    })),
+    }),
 
     // Function to be called with evaluated results from browsers
-    onSuccess: (result => {
+    onSuccess: (result) => {
       console.log(result?.result?.url);
-      const url = result?.result?.url;
-      const name = url.slice(url.lastIndexOf('/') + 1).replace('.aspx','').toLowerCase();
-      const html = result?.html;
+      // const url = result?.result?.url;
+      // const name = url.slice(url.lastIndexOf('/') + 1).replace('.aspx','').toLowerCase();
+      // const html = result?.html;
 
-      saveToFile(
-          name,
-          'html',
-          `${html}`,
-          './output/html'
-      );
-    }),
+      // saveToFile(
+      //     name,
+      //     '.html',
+      //     `${html}`,
+      //     './output/html'
+      // );
+    },
 
     onError: (error) => {
       console.error(error);
-    }
-
+    },
   });
 
   // Queue a request
   await crawler.queue({
-    url: 'https://partners.wgu.edu/Pages/Partners.aspx',
-    allowedDomains: [ 'partners.wgu.edu' ],
+    url: "https://partners.wgu.edu/Pages/PartnerInsts.aspx?sid=79",
+    allowedDomains: ["partners.wgu.edu"],
     delay: 1500,
     // waitFor: { selectorOrFunctionOrTimeout:  waitUntil: 'networkidle2', timeout: 60000 }
   });
   await crawler.onIdle(); // Resolved when no queue is left
   await crawler.close(); // Close the crawler
-  saveToFile('errors', 'json', JSON.stringify(errors), './output/json')
+  saveToFile("errors", ".json", JSON.stringify(errors), "./output/json");
 })();
